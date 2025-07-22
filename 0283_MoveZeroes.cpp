ed81bbf65b0abc5379002262ce2d9dccbf9f3e8c
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-07-22 16:17:54 +0530

    Day 5

---
+++ 0283_MoveZeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZeroIndex = 0;
+
+        // For Move all non zero elements are front
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                nums[lastNonZeroIndex++] = nums[i];
+            }
+        }
+
+        // Fill remaining positions with 0s
+        for(int i = lastNonZeroIndex; i < nums.size();i++){
+            nums[i] = 0;
+        }
+
+    }
+};
