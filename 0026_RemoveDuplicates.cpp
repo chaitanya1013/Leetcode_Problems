bdfc262620c0fa17ba28a82cc351db1e237fd419
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-07-25 23:40:42 +0530

    Day 8

--- 0026_RemoveDuplicates.cpp
+++ 0026_RemoveDuplicates.cpp
@@ -1,8 +1,8 @@
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int i = 0;
-        // int j = 1;
         for(int j=1;j<nums.size();j++){
             if(nums[j] != nums[i]){
                 i++;
