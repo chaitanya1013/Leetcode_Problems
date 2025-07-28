75ea1f37d887dae5334488498362320750309c1f
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-07-28 20:46:33 +0530

    Day 11

---
+++ 1295_findEvenNo.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        for(int i=0;i<nums.size();i++){
+            int num = nums[i];
+            int digitcount = 0;
+            while(num > 0){
+                num = num / 10;
+                digitcount++;
+            }
+            if(digitcount % 2 == 0){
+                count++;
+            }
+        }
+        // nums[i] 
+        return count;
+    }
+};
