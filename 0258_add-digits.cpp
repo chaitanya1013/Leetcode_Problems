56bb89fbb8fcd7f3ad8be347026eed9592dc165a
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-08-03 15:27:43 +0530

    Day 16

--- 0258_add-digits.cpp
+++ 0258_add-digits.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int addDigits(int num) {
-        
+        for (; num >= 10; ) {
+            int sum = 0;
+            for (int n = num; n > 0; n /= 10) {
+                sum += n % 10; 
+            }
+            num = sum; 
+        }
+        return num;
     }
 };
