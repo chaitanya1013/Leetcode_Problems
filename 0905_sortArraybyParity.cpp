3ca4c81e07bb1a4c37b83a8ed5c6ddff85a401e6
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-08-09 20:27:00 +0530

    Day 22

---
+++ 0905_sortArraybyParity.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int pos = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if(nums[i] % 2 == 0){
+                swap(nums[i],nums[pos]);
+                pos++;
+            }
+        }
+        return nums;
+    }
+};
