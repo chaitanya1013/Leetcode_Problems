9db8988725e9073cba466290ffba55fea3c5f8fb
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-08-16 17:09:51 +0530

    Day 6

--- 0485_MaxConsecutiveOnes.cpp
+++ 0485_MaxConsecutiveOnes.cpp
@@ -1,20 +1,18 @@
-
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int currentOnes = 0;                 // current counting ones
-        int maxCntOnes = 0;                  // Max count upto 
-        for(int i=0;i<nums.size();i++){      // loop check for array 
-            if(nums[i]==1){                  // if number is 1 then increase the current counting
-                currentOnes++;
+        int n = nums.size();
+        int max1 = 0;
+        int cnt = 0;
+        for(int i = 0 ; i < n; i++){
+            if(nums[i] == 1){
+                cnt++;
+                max1 = max(max1,cnt);
             }
-            else{                            // if number is not 1 then return current counting is 0
-                currentOnes = 0;
-            }
-            if(currentOnes>maxCntOnes){      // when current counting number is grater than max count then currentOnes will be maxCntOnes
-                maxCntOnes = currentOnes;
+            else{
+                cnt = 0;
             }
         }
-        return maxCntOnes;
+        return max1;
     }
 };
