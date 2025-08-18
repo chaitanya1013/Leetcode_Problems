915ea6a7372ddf2ad0cc0da9e2a0f43aa5535db0
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-08-18 22:38:01 +0530

    Day 29

---
+++ 0169_MajorityElement.better.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> mpp;
+        int n = nums.size();
+        for(int i = 0; i< n; i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it: mpp){
+            if(it.second > (nums.size() / 2)){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
--- 0169_MajorityElement.cpp
+++
@@ -1,7 +0,0 @@
-
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        
-    }
-};
