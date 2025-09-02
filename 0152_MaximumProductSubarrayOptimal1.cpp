116555f64a5f5de8f81b1153f0f0a6248b42b9d0
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-09-02 22:04:38 +0530

    Day 42

---
+++ 0152_MaximumProductSubarrayOptimal1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = INT_MIN;
+        int pref = 1 , suff = 1;
+        for(int i = 0; i< n; i++) {
+            if(pref == 0)    pref = 1;
+            if(suff == 0)    suff = 1;
+
+            pref = pref * nums[i];
+            suff = suff * nums[n-i-1]; 
+            maxi = max(maxi,max(pref,suff));    
+        }
+        return maxi;
+    }
+};
